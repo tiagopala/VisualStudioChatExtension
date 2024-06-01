28a63b9b912d3772f5fd421449eb793fc1ad6746
Tiago Pala <tiago_pala@outlook.com>
2024-06-01 15:04:10 -0300

    Added initial project

---
+++ src/VisualStudioChatExtension/Commands/MyCommand.cs
@@ -0,0 +1,11 @@
+﻿namespace VisualStudioChatExtension
+{
+    [Command(PackageIds.MyCommand)]
+    internal sealed class MyCommand : BaseCommand<MyCommand>
+    {
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            await VS.MessageBox.ShowWarningAsync("VisualStudioChatExtension", "Button clicked");
+        }
+    }
+}
